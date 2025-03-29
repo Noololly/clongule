69f8b1a657f421d118f50320073ad63170324393
noololly <oliver@stephan.co.uk>
2025-03-29 17:59:52 +0000

    Clongule Version 1!!

--- game.py
+++ game.py
@@ -8,224 +8,235 @@ import datetime
 
 
 def init(bot: discord.client):
-    """
-    Initialises the game so that all clongs and their words can be accessed by the program
-    :return:
-    """
-    global clongs, client
+	"""
+	Initialises the game so that all clongs and their words can be accessed by the program
+	:return:
+	"""
+	global clongs, client
 
-    client = bot
+	client = bot
 
-    conn = SQLConn()  #create the connection to the database
-    cur = conn.conn.cursor()
-    cur.execute("SELECT DISTINCT lang FROM clongule")  #get all individual clongs
-    clongs = [row[0] for row in cur.fetchall()]  #format it into a usable format
-    new_day()
+	conn = SQLConn()  #create the connection to the database
+	cur = conn.conn.cursor()
+	cur.execute("SELECT DISTINCT lang FROM clongule")  #get all individual clongs
+	clongs = [row[0] for row in cur.fetchall()]  #format it into a usable format
+	new_day()
 
 
 def new_day():
-    global day
-    day = Day()
+	global day
+	day = Day()
 
 
 class Day:
-    """
-    class that contains all the information about the word for that day
-    :return:
-    """
-    def __init__(self):
-        self.conn = SQLConn()
-        self.cur = self.conn.conn.cursor()
-        self.clong = self.get_lang()
-        self.word = self.get_word()
-        self.IPA = self.get_ipa()
-        self.english = self.get_english()
-        self.is_cxei = self.get_is_cxei()
+	"""
+	class that contains all the information about the word for that day
+	:return:
+	"""
+	def __init__(self):
+		self.conn = SQLConn()
+		self.cur = self.conn.conn.cursor()
+		self.clong = self.get_lang()
+		self.word = self.get_word()
+		self.IPA = self.get_ipa()
+		self.english = self.get_english()
+		self.is_cxei = self.get_is_cxei()
 
 
-    def get_english(self):
-        english_query = f"""
+	def get_english(self):
+		english_query = f"""
         SELECT eng FROM clongule
         WHERE word = %s
         LIMIT 1;
         """
-        self.cur.execute(english_query, (self.word,))
-        english = self.cur.fetchone()
-        return english[0]
+		self.cur.execute(english_query, (self.word,))
+		english = self.cur.fetchone()
+		return english[0]
 
 
-    def get_ipa(self) -> str: # gets IPA for the word of the day
-        IPA_query = f"""
+	def get_ipa(self) -> str: # gets IPA for the word of the day
+		IPA_query = f"""
         SELECT ipa FROM clongule
         WHERE word = %s
         LIMIT 1;
         """
-        self.cur.execute(IPA_query, (self.word,))
-        ipa = self.cur.fetchone()
-        return ipa[0]
+		self.cur.execute(IPA_query, (self.word,))
+		ipa = self.cur.fetchone()
+		return ipa[0]
 
 
-    def get_word(self) -> str: # gets the word of the day
-        word_query = f"""
+	def get_word(self) -> str: # gets the word of the day
+		word_query = f"""
         SELECT word FROM word
         WHERE lang = %s
         ORDER BY id DESC
         LIMIT 1;
         """
-        self.cur.execute(word_query, (self.clong,))
-        word = self.cur.fetchone()
-        return word[0]
+		self.cur.execute(word_query, (self.clong,))
+		word = self.cur.fetchone()
+		return word[0]
 
 
-    def get_lang(self) -> str: # gets the clong for the day
-        clong_query = """
+	def get_lang(self) -> str: # gets the clong for the day
+		clong_query = """
             SELECT lang FROM word
             ORDER BY id DESC
             LIMIT 1;
             """
-        self.cur.execute(clong_query)
-        clong = self.cur.fetchone()
-        return clong[0]
+		self.cur.execute(clong_query)
+		clong = self.cur.fetchone()
+		return clong[0]
 
 
-    def get_is_cxei(self):
-        cxei_query = f"""
+	def get_is_cxei(self):
+		cxei_query = f"""
         SELECT cxei FROM clongule
         WHERE lang = %s
         LIMIT 1;
         """
-        self.cur.execute(cxei_query, (self.clong,))
-        is_cxei = self.cur.fetchone()
-        return is_cxei[0]
+		self.cur.execute(cxei_query, (self.clong,))
+		is_cxei = self.cur.fetchone()
+		return is_cxei[0]
 
 
 class GuessButton(Button):
-    """
-    A class that contains the submit button
-    :return:
-    """
+	"""
+	A class that contains the submit button
+	:return:
+	"""
 
-    def __init__(self, parent_view):
-        super().__init__(label="Submit", style=discord.ButtonStyle.success)
-        self.parent_view = parent_view
+	def __init__(self, parent_view):
+		super().__init__(label="Submit", style=discord.ButtonStyle.success)
+		self.parent_view = parent_view
 
 
-    async def callback(self, interaction: discord.Interaction):
-        selected = self.parent_view.guess.selected_value
-        if selected:
-            await interaction.response.defer()
-            channel = interaction.channel
-            user_id = interaction.user.id
-            await validate_guess(clongs[int(selected)-1], channel, user_id) # selected is a number in a string for some dumb reason
-        else:
-            await interaction.response.send_message("Select an option first!", ephemeral=True)
+	async def callback(self, interaction: discord.Interaction):
+		selected = self.parent_view.guess.selected_value
+		if selected:
+			await interaction.response.defer()
+			channel = interaction.channel
+			user_id = interaction.user.id
+			await validate_guess(clongs[int(selected)-1], channel, user_id) # selected is a number in a string for some dumb reason
+		else:
+			await interaction.response.send_message("Select an option first!", ephemeral=True)
 
 
 class Guess(Select):
-    """
-    A class that contains the clong selection list
-    :return:
-    """
+	"""
+	A class that contains the clong selection list
+	:return:
+	"""
 
-    def __init__(self):
-        super().__init__(
-            min_values=1,
-            max_values=1,
-            placeholder="Select your guess...",
-            options=[discord.SelectOption(label=clong, value=str(i + 1)) for i, clong in enumerate(clongs)]
-        )
-        self.selected_value = None
+	def __init__(self):
+		super().__init__(
+			min_values=1,
+			max_values=1,
+			placeholder="Select your guess...",
+			options=[discord.SelectOption(label=clong, value=str(i + 1)) for i, clong in enumerate(clongs)]
+		)
+		self.selected_value = None
 
 
-    async def callback(self, interaction: discord.Interaction):
-        await interaction.response.defer()
-        self.selected_value = self.values[0]
+	async def callback(self, interaction: discord.Interaction):
+		await interaction.response.defer()
+		self.selected_value = self.values[0]
 
 
 class GuessView(View):
-    """
-    A class that allows all the components of a guess to be sent in one message, and so that they can interact with each other
-    :return:
-    """
+	"""
+	A class that allows all the components of a guess to be sent in one message, and so that they can interact with each other
+	:return:
+	"""
 
-    def __init__(self):
-        super().__init__()
-        self.guess = Guess()
-        self.add_item(self.guess)
-        self.add_item(GuessButton(self))
+	def __init__(self):
+		super().__init__()
+		self.guess = Guess()
+		self.add_item(self.guess)
+		self.add_item(GuessButton(self))
 
 
 async def validate_guess(guess, channel: discord.TextChannel, user_id: int):
-    """
-    Function to validate the user's guess
-    :param guess:
-    :param channel:
-    :param user_id:
-    :return:
-    """
-    is_cxei_query = """
+	"""
+	Function to validate the user's guess
+	:param guess:
+	:param channel:
+	:param user_id:
+	:return:
+	"""
+
+	user = await client.fetch_user(user_id)
+
+	is_cxei_query = """
     SELECT cxei from clongule
     WHERE lang = %s
     LIMIT 1;
     """
-    conn = SQLConn()
-    cur = conn.conn.cursor()
-    cur.execute(is_cxei_query, (guess,))
-    is_cxei = cur.fetchone()[0]
+	conn = SQLConn()
+	cur = conn.conn.cursor()
+	cur.execute(is_cxei_query, (guess,))
+	is_cxei = cur.fetchone()[0]
 
-    if guess == day.clong:
-        await channel.send("Correct!")
-        if user_id in user_guesses:
-            del user_guesses[user_id]
-    else:
-        if user_id in user_guesses:
-            user_guesses[user_id] = (user_guesses[user_id][0] + 1, time.time())
-        else:
-            user_guesses[user_id] = (1, time.time())
+	if user_id not in timed_out:
+		if guess == day.clong:
+			user = await client.fetch_user(user_id)
+			await channel.send(f"{user.mention} Correct!")
+			if user_id in user_guesses:
+				del user_guesses[user_id]
+			timed_out[user_id] = time.time()
+		else:
+			if user_id in user_guesses:
+				user_guesses[user_id] = (user_guesses[user_id][0] + 1, time.time())
+			else:
+				user_guesses[user_id] = (1, time.time())
 
-        if user_guesses[user_id][0] <= 2:
-            await channel.send(f"""Incorrect! Try again. You are on guess {user_guesses[user_id][0]} out of 3. Cxei: {'🟩' if is_cxei and day.is_cxei else '⬛'}""")
-        # ^^ sends the user a message telling them they are incorrect and how many guesses they have left and if their answer is correctly cxei or not
+			if user_guesses[user_id][0] <= 2:
+				await channel.send(f"""{user.mention} Incorrect! Try again. You are on guess {user_guesses[user_id][0]} out of 3. Cxei or not: {'🟩' if is_cxei and day.is_cxei or not is_cxei and not day.is_cxei else '⬛'}""")
+			# ^^ sends the user a message telling them they are incorrect and how many guesses they have left and if their answer is correctly cxei or not
 
-        else:
-            await channel.send("Fuck u lol")
+			else:
+				await channel.send(f"{user.mention} Incorrect! Come back tomorrow!")
+				del user_guesses[user_id]
+				timed_out[user_id] = time.time()
+	else:
+		await channel.send(f"{user.mention} Come back tomorrow!")
 
 
 async def remove_time_out_nanny():
-    """
-    Function to wait until the next quarter of a day and then remove all users who have timed out
-    :return:
-    """
-    while True:
-        now = datetime.datetime.now()
-        if 0 <= now.hour < 6:
-            target_time = now.replace(hour=6, minute=0, second=0, microsecond=0)
-        elif 6 <= now.hour < 12:
-            target_time = now.replace(hour=12, minute=0, second=0, microsecond=0)
-        elif 12 <= now.hour < 18:
-            target_time = now.replace(hour=18, minute=0, second=0, microsecond=0)
-        else:
-            target_time = now.replace(hour=0, minute=0, second=0, microsecond=0) + datetime.timedelta(days=1)
+	"""
+	Function to wait until the next quarter of a day and then remove all users who have timed out
+	:return:
+	"""
+	while True:
+		now = datetime.datetime.now()
+		if 0 <= now.hour < 6:
+			target_time = now.replace(hour=6, minute=0, second=0, microsecond=0)
+		elif 6 <= now.hour < 12:
+			target_time = now.replace(hour=12, minute=0, second=0, microsecond=0)
+		elif 12 <= now.hour < 18:
+			target_time = now.replace(hour=18, minute=0, second=0, microsecond=0)
+		else:
+			target_time = now.replace(hour=0, minute=0, second=0, microsecond=0) + datetime.timedelta(days=1)
 
-        sleep_time = (target_time - now).total_seconds()
-        await asyncio.sleep(sleep_time)
+		sleep_time = (target_time - now).total_seconds()
+		await asyncio.sleep(sleep_time)
 
-        await remove_timed_out()
+		await remove_timed_out()
 
 
 async def remove_timed_out():
-    """
-    Removes all timed out users
-    :return:
-    """
-    current_time = time.time()
-    timeout = 6 * 60 * 60 # 6 hour time out
-    to_remove = [user_id for user_id, (_, last_guess_time) in user_guesses.items() if current_time - last_guess_time > timeout] # gets the users which have been inactive for over 6 hours
-    for user_id in to_remove:
-        del user_guesses[user_id]
-        user_to_dm =  await client.fetch_user(user_id)
-        await user_to_dm.send("Your guess has timed out.")
+	"""
+	Removes all timed out users
+	:return:
+	"""
+	current_time = time.time()
+	timeout = 6 * 60 * 60 # 6 hour time out
+	to_remove = [user_id for user_id, (_, last_guess_time) in user_guesses.items() if current_time - last_guess_time > timeout] # gets the users which have been inactive for over 6 hours
+	for user_id in to_remove:
+		del user_guesses[user_id]
 
+	to_remove = [user_id for user_id, last_guess_time in timed_out.items() if current_time - last_guess_time > timeout] # gets the users which have been inactive for over 6 hours
+	for user_id in to_remove:
+		del timed_out[user_id]
 
 day: Optional[Day] = None # tells intellisense to stop whining
 client: Optional[discord.Client] = None
@@ -233,3 +244,4 @@ client: Optional[discord.Client] = None
 words = {}
 clongs = []
 user_guesses = {}
+timed_out = {}
--- main.py
+++ main.py
@@ -20,42 +20,42 @@ client = commands.Bot(command_prefix="c/", intents=intents)
 
 @client.event
 async def on_ready():
-    print(f"Logged in as {client.user}")
-    game.init(client)
-    asyncio.create_task(scheduled_task())
+	print(f"Logged in as {client.user}")
+	game.init(client)
+	asyncio.create_task(scheduled_task())
 
 
 @client.command(name="clongule")
 async def send_message(ctx):
-    view = game.GuessView()
-    await ctx.send(f"Word: {game.day.word}\nIPA: {game.day.IPA}\nEnglish: {game.day.english}", view=view)
+	view = game.GuessView()
+	await ctx.send(f"Word: {game.day.word}\nIPA: {game.day.IPA}\nEnglish: {game.day.english}", view=view)
 
 
 @client.command(name="test")
 async def send_message(_):
-    game.new_day()
-    await alert_new_word()
+	game.new_day()
+	await alert_new_word()
 
 
 async def scheduled_task():
-    """Runs every day at 00:00"""
-    while True:
-        now = datetime.datetime.now()
-        target_time = now.replace(hour=0, minute=0, second=0, microsecond=0)
+	"""Runs every day at 00:00"""
+	while True:
+		now = datetime.datetime.now()
+		target_time = now.replace(hour=0, minute=0, second=0, microsecond=0)
 
-        if now >= target_time:
-            target_time += datetime.timedelta(days=1)
+		if now >= target_time:
+			target_time += datetime.timedelta(days=1)
 
-        sleep_seconds = (target_time - now).total_seconds() # calculate time until midnight
-        await asyncio.sleep(sleep_seconds) # wait until midnight
+		sleep_seconds = (target_time - now).total_seconds() # calculate time until midnight
+		await asyncio.sleep(sleep_seconds) # wait until midnight
 
-        await alert_new_word() # once midnight, alert new word
+		await alert_new_word() # once midnight, alert new word
 
 
 async def alert_new_word():
-    aisje_clongule_channel_id = 1347631529471508560
-    clongule_channel = await client.fetch_channel(aisje_clongule_channel_id)
-    await clongule_channel.send("New word available!!")
+	aisje_clongule_channel_id = 1347631529471508560
+	clongule_channel = await client.fetch_channel(aisje_clongule_channel_id)
+	await clongule_channel.send("New word available!!")
 
 
 client.run(BOT_TOKEN)
